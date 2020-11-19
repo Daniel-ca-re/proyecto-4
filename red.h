e72faaaee5ed573f587cc4ef05e9c512ab7e2208
Daniel Cano <Daniel.canor1@udea.edu.co>
2020-11-19 17:34:03 -0500

    0

---
+++ red.h
@@ -0,0 +1,16 @@
+#ifndef RED_H
+#define RED_H
+
+#include <QObject>
+
+class red : public QObject
+{
+    Q_OBJECT
+public:
+    explicit red(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // RED_H
